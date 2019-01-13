fb852bcf7e744a0c2c0335f102e0ae58fed0aef0
kimmikimmi <bright20136@gmail.com>
2019-01-13 16:25:51 +0900

    1. 스웨거 작업 및 auth 해제

--- src/main/kotlin/com/jworkplace/demo/auth/RequestTokenAuthInterceptor.kt
+++ src/main/kotlin/com/jworkplace/demo/auth/RequestTokenAuthInterceptor.kt
@@ -17,17 +17,17 @@ class RequestTokenAuthInterceptor(@Autowired val firebaseAuthManager: FirebaseAu
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         return true
         //개발 환경에서 테스트할때 사용하려고 우선 만들어 둠, 추후에 환경을 분리하여 적용해 보자.
-        if (request.getHeader("env") != null && request.getHeader("env").contains("develop")) {
-            return true
-        }
-        val token = request.getHeader("token")
-        try {
-            val uuid = firebaseAuthManager.verifyToken(token)
-            log.info("token=$token , uuid=$uuid")
-        } catch (e: java.lang.Exception) {
-            log.error("token: $token is not valid", e)
-            return false
-        }
-        return true
+//        if (request.getHeader("env") != null && request.getHeader("env").contains("develop")) {
+//            return true
+//        }
+//        val token = request.getHeader("token")
+//        try {
+//            val uuid = firebaseAuthManager.verifyToken(token)
+//            log.info("token=$token , uuid=$uuid")
+//        } catch (e: java.lang.Exception) {
+//            log.error("token: $token is not valid", e)
+//            return false
+//        }
+//        return true
     }
 }
